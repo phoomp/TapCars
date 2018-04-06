b23019e9ac114b75da7625adf9cbd8bb949c12a2
Phoom <phoomp14@gmail.com>
2018-04-06 21:12:00 +0700

    Initial repository

--- TapCars/GameScene.swift
+++ TapCars/GameScene.swift
@@ -11,79 +11,141 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var touchCount: Int = 0
+    var leftCar = SKSpriteNode()
+    var rightCar = SKSpriteNode()
+    var canMove = false
+    var leftToMoveLeft = true
+    var rightCarToMoveRight = true
+    var leftCarAtRight = false
+    var rightCarAtLeft = false
+    var centerPoint : CGFloat!
+    
+    let leftCarMinimumX :CGFloat = -280
+    let leftCarMaximumX :CGFloat = -100
+    
+    let rightCarMinimumX :CGFloat = 100
+    let rightCarMaximumX :CGFloat = 200
+    
+    var roadStrip: [SKShapeNode] = []
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        setUp()
+        Timer.scheduledTimer(timeInterval: TimeInterval(0.1), target: self, selector: #selector(GameScene.createRoadStrip), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameScene.removeItems), userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            if touchLocation.x > centerPoint {
+                if rightCarAtLeft {
+                    rightCarAtLeft = false
+                    rightCarToMoveRight = true
+                }
+                else {
+                    rightCarAtLeft = true
+                    rightCarToMoveRight = false
+                }
+            }
+            else {
+                if leftCarAtRight {
+                    leftCarAtRight = false
+                    leftToMoveLeft = true
+                }
+                else {
+                    leftCarAtRight = true
+                    leftToMoveLeft = false
+                }
+            }
+            canMove = true
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if canMove {
+            move(leftSide:leftToMoveLeft)
+        }
+        showRoadStrip()
     }
+    
+    func setUp() {
+        leftCar = self.childNode(withName: "leftCar") as! SKSpriteNode
+        rightCar = self.childNode(withName: "rightCar") as! SKSpriteNode
+        centerPoint = self.frame.size.width / self.frame.size.height
+    }
+    
+    @objc func createRoadStrip() {
+        let leftRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
+        leftRoadStrip.strokeColor = SKColor.white
+        leftRoadStrip.fillColor = SKColor.white
+        leftRoadStrip.alpha = 0.4
+        leftRoadStrip.name = "leftRoadStrip"
+        leftRoadStrip.zPosition = 10
+        leftRoadStrip.position.x = -187.5
+        leftRoadStrip.position.y = 700
+        addChild(leftRoadStrip)
+        
+        let rightRoadStrip = SKShapeNode(rectOf: CGSize(width: 10, height: 40))
+        rightRoadStrip.strokeColor = SKColor.white
+        rightRoadStrip.fillColor = SKColor.white
+        rightRoadStrip.alpha = 0.4
+        rightRoadStrip.name = "rightRoadStrip"
+        rightRoadStrip.zPosition = 10
+        rightRoadStrip.position.x = 187.5
+        rightRoadStrip.position.y = 700
+        addChild(rightRoadStrip)
+    }
+    
+    func showRoadStrip() {
+        enumerateChildNodes(withName: "leftRoadStrip") { (roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        }
+        enumerateChildNodes(withName: "rightRoadStrip") { (roadStrip, stop) in
+            let strip = roadStrip as! SKShapeNode
+            strip.position.y -= 30
+        }
+    }
+    
+    @objc func removeItems() {
+        for child in children {
+            if child.position.y < -self.size.height - 15 {
+                child.removeFromParent()
+            }
+        }
+    }
+    
+    func move(leftSide:Bool) {
+        if leftSide {
+            leftCar.position.x -= 20
+            if leftCar.position.x < leftCarMinimumX {
+                leftCar.position.x = leftCarMinimumX
+            }
+        }
+        else {
+            leftCar.position.x += 20
+            if leftCar.position.x > leftCarMaximumX {
+                leftCar.position.x = leftCarMaximumX
+            }
+        }
+    }
+    func moveRightCar(rightSide:Bool) {
+        if rightSide {
+            rightCar.position.x += 20
+            if rightCar.position.x > rightCarMaximumX {
+                rightCar.position.x = rightCarMaximumX
+            }
+        }
+        else {
+            rightCar.position.x -= 20
+            if rightCar.position.x < rightCarMinimumX {
+                rightCar.position.x = rightCarMinimumX
+            }
+        }
+    }
+    
 }
+    
+
